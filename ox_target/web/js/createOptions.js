bdf79b93acefd7fe15c09f81fcbacda5489d522e
Kevin Boon <kevin_boon@outlook.com>
2023-08-05 14:32:03 -0400

    Updated To Support New Overlapping Zones
    Updated To Support New Overlapping Zones

--- ox_target/web/js/createOptions.js
+++ ox_target/web/js/createOptions.js
@@ -2,10 +2,24 @@ import { fetchNui } from './fetchNui.js';
 
 const optionsWrapper = document.getElementById('options-wrapper');
 
-export function createOptions(type, data, id) {
+function onClick() {
+  // when nuifocus is disabled after a click, the hover event is never released
+  this.style.pointerEvents = "none";
+
+  fetchNui("select", [this.targetType, this.targetId, this.zoneId]);
+  // is there a better way to handle this? probably
+  setTimeout(() => (this.style.pointerEvents = "auto"), 100);
+}
+
+export function createOptions(type, data, id, zoneId) {
   if (data.hide) return;
   const option = document.createElement('li');
+
   option.innerHTML = `<a><i class="fa-fw ${data.icon} option-icon" style="color:${data.iconColor || '#2d2f32'}"></i>${data.label}</a>`;
-  option.addEventListener('click', () => fetchNui('select', [type, id]));
+  option.targetType = type;
+  option.targetId = id;
+  option.zoneId = zoneId;
+
+  option.addEventListener("click", onClick);
   optionsWrapper.appendChild(option);
 }
